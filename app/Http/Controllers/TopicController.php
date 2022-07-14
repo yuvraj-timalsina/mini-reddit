72e56e96df37b561764eb6bfd2c33594ed92eb16
timalsina-yuvraj <timalsinayuvraj@gmail.com>
2022-07-14 10:00:06 +0545

    Topic MCRFS Created

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTopicRequest;
+use App\Http\Requests\UpdateTopicRequest;
+use App\Models\Topic;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTopicRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTopicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTopicRequest  $request
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTopicRequest $request, Topic $topic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Topic  $topic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topic $topic)
+    {
+        //
+    }
+}
--- resources/views/communities/create.blade.php
+++ resources/views/communities/create.blade.php
@@ -1,17 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('New Community') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('New Community') }}</div>
 
-                <div class="card-body">
-                   Form here
+                    <div class="card-body">
+
+                        <form method="POST" action="{{ route('communities.store') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text"
+                                           class="form-control @error('name') is-invalid @enderror" name="name"
+                                           value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="description"
+                                       class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
+
+                                <div class="col-md-6">
+                                    <textarea class="form-control @error('description') is-invalid @enderror"
+                                              name="description" id="description"
+                                              required>{{old('description')}}</textarea>
+
+                                    @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Create Community') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
